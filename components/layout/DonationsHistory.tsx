23ca53ff7dfc844490ecd94b6c5afa1bd46c0b7b
martijn <martijncvv@gmail.com>
2024-09-13 19:46:07 +0200

    - prettier and eslint

---
+++ components/layout/DonationsHistory.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+interface DonationsHistoryProps {
+  // TODO
+}
+
+export const DonationsHistory: React.FC<DonationsHistoryProps> = () => {
+  return (
+    <View>
+      <Text>Donations History</Text>
+    </View>
+  );
+};
