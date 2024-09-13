1ff15ae89fe20ba901276dedb09bb334c4c4162e
martijn <martijncvv@gmail.com>
2024-09-13 19:48:50 +0200

    - setup donations page - setup react query

--- components/layout/DonationsHistory.tsx
+++ components/layout/DonationsHistory.tsx
@@ -1,4 +1,6 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { Styles } from "@/assets/constants/Styles";
+import { Colors } from "@/assets/constants/Colors";
 
 interface DonationsHistoryProps {
   // TODO
@@ -7,7 +9,14 @@ interface DonationsHistoryProps {
 export const DonationsHistory: React.FC<DonationsHistoryProps> = () => {
   return (
     <View>
-      <Text>Donations History</Text>
+      <Text style={styles.title}>Donations History</Text>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  title: {
+    fontSize: Styles.typography.fontSize.xxl,
+    fontWeight: Styles.typography.fontWeight.extraBold,
+    color: Colors.neutrals.black,
+  },
+});
